2929ee2c2851a5e89e5caf2aebaf4acc0445859a
Angel Peralta <amiguelp007@gmail.com>
2020-11-16 22:34:39 -0500

    11/16/2020 GUI

---
+++ airport_marker.py
@@ -0,0 +1,47 @@
+from kivy_garden.mapview import MapMarkerPopup
+import sys
+
+class AirportMarker(MapMarkerPopup):
+    source = 'map_marker.png'
+
+    def __init__(self, data):
+        super(AirportMarker, self).__init__()
+        self.data = data
+        self.lat = data[15]
+        self.lon = data[16]
+
+    def on_release(self):
+        # Open up the LocationsPopupMenu
+        pass
+
+'''
+import sqlite3
+from data_structures import AVLTree
+
+connection = sqlite3.connect("global_airports.db")
+
+cursor = connection.cursor()
+
+#cursor.execute("SELECT * FROM AIRPORTS WHERE NAME LIKE '%N/A%'")
+cursor.execute("SELECT * FROM airports")
+
+result = cursor.fetchall()
+print(result)
+
+tree = AVLTree(2)
+root = None
+
+
+for index in range(0, len(result)):
+    if result[index][tree.index] == r'N/A':
+        continue
+    lat, lon = result[index][15], result[index][16]
+    root = tree.insert_node(root, AirportMarker(data=list(result[index]), lat=lat, lon=lon))
+    print(result[index][tree.index])
+tree.printHelper(root, "", True)
+'''
+
+
+
+
+
